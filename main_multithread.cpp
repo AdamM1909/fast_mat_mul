f2f3154d622c75e853e3960d070ed9c7015f0354
Adam Myers <adam.myers1909@gmail.com>
2024-02-17 21:04:29 +0000

    Added grid search for inner tile size.

--- main_multithread.cpp
+++ main_multithread.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <stdexcept>
 #include "matrix_utils.hpp" 
-#include "mat_mul_implementations.hpp"
+#include "mat_mul_implementations_multithread.hpp"
 #include "benchmark/benchmark.h"
 
 // g++ main.cpp -std=c++11 -lbenchmark -lpthread -o main -march=native -ffast-math -O3 -Xpreprocessor -fopenmp -I/opt/homebrew/Cellar/libomp/17.0.6/include -L/opt/homebrew/Cellar/libomp/17.0.6/lib -lomp
+
 static void BM_matmulImpl(benchmark::State& state) {
     // Create matrices and result matrix
     const int rows = 1024;
@@ -20,15 +21,22 @@ static void BM_matmulImpl(benchmark::State& state) {
     load_matrix(left, "matrix_a.bin", rows, columns);
     load_matrix(right, "matrix_b.bin", rows, columns);
 
+
     for (auto _ : state) {
         state.PauseTiming();
         std::fill_n(result, rows * columns, 0);
         state.ResumeTiming();
-        const int inner_tile_size = 16;
-        // mat_mul_tiled_1d<rows, columns, inner>(left, right, result, inner_tile_size);
-        mat_mul_tiled_3d_multithread<rows, columns, inner, inner_tile_size>(left, right, result);
+        const int inner_tile_size = state.range(0);
+        // Cases must be templates for multthreading compilation. 
+        switch(inner_tile_size){
+            case 8: mat_mul_tiled_3d_multithread<rows, columns, inner, 8, 128>(left, right, result); break;
+            case 16: mat_mul_tiled_3d_multithread<rows, columns, inner, 16, 128>(left, right, result); break;
+            case 32: mat_mul_tiled_3d_multithread<rows, columns, inner, 32, 128>(left, right, result); break;
+            case 64: mat_mul_tiled_3d_multithread<rows, columns, inner, 64, 128>(left, right, result); break;
+            case 128: mat_mul_tiled_3d_multithread<rows, columns, inner, 128, 128>(left, right, result); break;
+            default: break;
         }
-    
+    }
     // Check 
     load_matrix(check_result, "matrix_c.bin", rows, columns);
     bool arrays_match = compare_matrices(check_result, result, rows*columns);
@@ -43,7 +51,7 @@ static void BM_matmulImpl(benchmark::State& state) {
 
 BENCHMARK(BM_matmulImpl)
     ->Unit(benchmark::kMillisecond)
-    // ->Arg(19)->Arg(20)->Arg(21)->Arg(22)->Arg(23)->Arg(24)
+    ->RangeMultiplier(2)->Range(8, 128)
     ->UseRealTime();
 
 BENCHMARK_MAIN();
--- mat_mul_implementations_multithread.hpp
+++ mat_mul_implementations_multithread.hpp
@@ -3,22 +3,22 @@
 
 #include <omp.h>
 
-template <int rows, int columns, int inners, int inner_tile_size>
+template <int rows, int columns, int inners, int inner_tile_size, int outer_tile_size>
 inline void mat_mul_tiled_3d_multithread(const float *left,
                                          const float *right,
                                          float *result) {
 
   #pragma omp parallel for shared(result, left, right) default(none) collapse(2) num_threads(8)
-  for (int rowTile = 0; rowTile < rows; rowTile += 128) {
-    for (int columnTile = 0; columnTile < columns; columnTile += 128) {
-
+  //   Split the matrix into mini matricies (outer_tile_size X outer_tile_size) for each thread to handle.
+  for (int rowTile = 0; rowTile < rows; rowTile += outer_tile_size) {
+    for (int columnTile = 0; columnTile < columns; columnTile += outer_tile_size) {
+      
+      // Now apply the inner tiling as before.                   
       for (int innerTile = 0; innerTile < inners; innerTile += inner_tile_size) {
-        // Matrix loops.
-        for (int row = rowTile; row < rowTile + 128; row++) {
+        for (int row = rowTile; row < rowTile + outer_tile_size; row++) {
           int innerTileEnd = std::min(inners, innerTile + inner_tile_size);
           for (int inner = innerTile; inner < innerTileEnd; inner++) {
-            for (int col = columnTile; col < columnTile + 128; col++) {
-              
+            for (int col = columnTile; col < columnTile + outer_tile_size; col++) {
               result[row * columns + col] += left[row * inners + inner] * right[inner * columns + col];
             } 
           }
